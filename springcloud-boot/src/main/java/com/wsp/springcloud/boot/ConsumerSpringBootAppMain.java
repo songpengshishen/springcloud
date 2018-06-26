62380e9e4e4aced06bbe8f3c39626693a6d78ae3
wangsongpeng <wangsongpeng@jd.com>
2018-06-26 11:21:13 +0800

    spring cloud 注册中心 ribbon软负载均衡框架

--- springcloud-boot/src/main/java/com/wsp/springcloud/boot/ConsumerSpringBootAppMain.java
+++ springcloud-boot/src/main/java/com/wsp/springcloud/boot/ConsumerSpringBootAppMain.java
@@ -3,13 +3,14 @@ package com.wsp.springcloud.boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication(scanBasePackages={"com.wsp"})
 @EnableDiscoveryClient
 public class ConsumerSpringBootAppMain {
-
+    @LoadBalanced
     @Bean
     public RestTemplate restTemplate(){
         return new RestTemplate();
--- springcloud-web/src/main/java/com/wsp/springcloud/web/controller/MovieController.java
+++ springcloud-web/src/main/java/com/wsp/springcloud/web/controller/MovieController.java
@@ -1,7 +1,11 @@
 package com.wsp.springcloud.web.controller;
 
 import com.wsp.springcloud.consumer.domain.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +15,22 @@ import org.springframework.web.client.RestTemplate;
 public class MovieController {
     @Autowired
     private RestTemplate restTemplate;
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+    private static final Logger logger = LoggerFactory.getLogger(MovieController.class);
+
     @GetMapping("/user/{id}")
     public User findById(@PathVariable Long id){
-         return this.restTemplate.getForObject("http://localhost:8000/"+id,User.class);
+         return this.restTemplate.getForObject("http://springcloud-provider-user/"+id,User.class);
     }
+
+    @GetMapping("/instance")
+    public void findInstance(){
+       ServiceInstance serviceInstance =  this.loadBalancerClient.choose("springcloud-provider-user");
+       logger.info("{}:{}:{}",serviceInstance.getServiceId(),serviceInstance.getHost(),serviceInstance.getPort());
+    }
+
+
 }
